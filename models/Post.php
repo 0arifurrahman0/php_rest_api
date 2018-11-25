f388c28b5000200c2f6bb883e3e94d80a4f15250
arif <guest.batworld@gmail.com>
2018-11-26 00:49:13 +0600

    API create

--- models/Post.php
+++ models/Post.php
@@ -63,4 +63,34 @@ class Post
         $stmt->execute();
         return $stmt;
     }
+
+    public function create()
+    {
+        $query = 'INSERT INTO
+                    '. $this->table .'
+                  SET
+                    category_id = :category_id,
+                    title = :title,
+                    body = :body,
+                    author = :author';
+
+        $stmt = $this->connection->prepare($query);
+
+        $this->title        = htmlspecialchars(strip_tags($this->title));
+        $this->body         = htmlspecialchars(strip_tags($this->body));
+        $this->author       = htmlspecialchars(strip_tags($this->author));
+        $this->created_at   = htmlspecialchars(strip_tags($this->category_id));
+
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':body', $this->body);
+        $stmt->bindParam(':author', $this->author);
+        $stmt->bindParam(':category_id', $this->category_id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        printf("Error!: %s.\n", $stmt->error);
+        return false;
+    }
 }
